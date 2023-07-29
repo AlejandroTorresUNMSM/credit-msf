3080d474fe01d92888054202b9bb57c47b686a37
AlejandroTorresUNSMS <alephdmc2575@gmail.com>
2023-07-28 20:16:26 -0500

    dev: commit inicial

---
+++ src/main/java/com/atorres/nttdata/creditmsf/CreditMsfApplication.java
@@ -0,0 +1,13 @@
+package com.atorres.nttdata.creditmsf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditMsfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditMsfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/atorres/nttdata/creditmsf/CreditMsfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atorres.nttdata.creditmsf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditMsfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
